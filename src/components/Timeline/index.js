54d1133d7f71c691a05c7e80f35076e84381b350
Luk4x <luk4xm4ci3l@gmail.com>
2022-11-13 09:13:03 -0300

    youtube api implemented

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,8 +1,40 @@
+import { useEffect, useState } from 'react';
 import config from '../../../config.json';
 import { StyledTimeline } from './styles';
 
 export default function Timeline({ searchValue }) {
     const playlistNames = Object.keys(config.playlists);
+    const [playlists, setPlaylists] = useState('');
+
+    console.log();
+
+    useEffect(() => {
+        fetch(
+            `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=UC2fVSthyWxWSjsiEAHPzriQ&maxResults=25&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`
+        )
+            .then(data => data.json())
+            .then(list => {
+                console.log(list.items);
+                setPlaylists(list.items);
+            });
+    }, []);
+
+    /*
+    return (
+        <StyledTimeline>
+            {playlists.map(playlist => {
+                return (
+                    <section key={playlist.id}>
+                        <h3>{playlist.snippet.title}</h3>
+                        <div>
+                            <img src={playlist.snippet.thumbnails.medium.url} alt="video thumb" />
+                        </div>
+                    </section>
+                );
+            })}
+        </StyledTimeline>
+    )
+    */
 
     return (
         <StyledTimeline>
